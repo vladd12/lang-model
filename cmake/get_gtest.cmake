285ff8050db699da796ce611b5fb922490cfbb42
vladd12 <vladd12z32@gmail.com>
2023-03-13 11:07:06 +0300

    Initial project commit

---
+++ cmake/get_gtest.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+
+# Getting googletest sources from GitHub repository
+FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG        origin/v1.12.x
+)
+
+# Compiling googletest library
+FetchContent_MakeAvailable(googletest)
+
+# Import as GTest::GTest package
+add_library(GTest::GTest INTERFACE IMPORTED)
+target_link_libraries(GTest::GTest INTERFACE gtest_main)
