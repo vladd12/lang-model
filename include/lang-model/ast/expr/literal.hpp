d6a3d10fc2a96b6d9c783df643425852f0ceea6f
vladd12 <vladd12z32@gmail.com>
2024-12-27 19:19:17 +0300

    Update for literal expression

--- include/lang-model/ast/expr/literal.hpp
+++ include/lang-model/ast/expr/literal.hpp
@@ -1,16 +1,32 @@
 #pragma once
 
 #include <lang-model/ast/expression.hpp>
+#include <string>
+#include <variant>
 
 namespace AST
 {
 
-/// TODO: One class for integer, float and string literals?
-// "Literal : Object value"
 class Literal final : public Expression
 {
 public:
-  Literal() noexcept;
+  using holder_type = std::variant<std::uint64_t, std::int64_t, double, bool, char, std::string>;
+
+private:
+  holder_type m_value;
+
+public:
+  explicit Literal() = delete;
+
+  Literal(const std::uint64_t value) noexcept;
+  Literal(const std::int64_t value) noexcept;
+  Literal(const double value) noexcept;
+  Literal(const bool value) noexcept;
+  Literal(const char value) noexcept;
+  Literal(const std::string &value);
+
+  holder_type &get() noexcept;
+  const holder_type &get() const noexcept;
 
   void accept(Visitor &visitor) override;
 };
--- src/ast/expr/literal.cpp
+++ src/ast/expr/literal.cpp
@@ -5,7 +5,39 @@
 namespace AST
 {
 
-Literal::Literal() noexcept = default;
+Literal::Literal(const std::uint64_t value) noexcept : Expression(), m_value(value)
+{
+}
+
+Literal::Literal(const std::int64_t value) noexcept : Expression(), m_value(value)
+{
+}
+
+Literal::Literal(const double value) noexcept : Expression(), m_value(value)
+{
+}
+
+Literal::Literal(const bool value) noexcept : Expression(), m_value(value)
+{
+}
+
+Literal::Literal(const char value) noexcept : Expression(), m_value(value)
+{
+}
+
+Literal::Literal(const std::string &value) : Expression(), m_value(value)
+{
+}
+
+Literal::holder_type &Literal::get() noexcept
+{
+  return m_value;
+}
+
+const Literal::holder_type &Literal::get() const noexcept
+{
+  return m_value;
+}
 
 void Literal::accept(Visitor &visitor)
 {
